2266ae251addd3498d7505148fd126695b605e86
Mikhail Ivkov <mivkov@maprtech.com>
2017-09-09 17:40:08 -0400

    Fix Readme and clean up functions

--- pupil.py
+++ pupil.py
@@ -1,10 +1,8 @@
-# Identify pupils. Based on beta 1
+# Inspired and made from https://gist.github.com/edfungus/67c14af0d5afaae5b18c
 
 import numpy as np
 import cv2
-import time
 import math
-import os
 
 def capturePupil(cxpts):
     '''
@@ -24,9 +22,10 @@ def capturePupil(cxpts):
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
             eyes = cv2.CascadeClassifier('haarcascade_eye.xml')
+            #  https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
             detected = eyes.detectMultiScale(frame, 1.3, 5)
 
-            frame = cv2.bilateralFilter(frame, 11, 17, 17)
+            frame = cv2.bilateralFilter(frame, 11, 17, 17) # by smoothing, make edges more visible
 
 
             pupilFrame = frame
@@ -40,11 +39,12 @@ def capturePupil(cxpts):
 
                 cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (0, 0, 255), 1)
                 cv2.line(frame, (x, y), ((x + w, y + h)), (0, 0, 255), 1)
-                cv2.line(frame, (x + w, y), ((x, y + h)), (0, 0, 255), 1)
-                pupilFrame = cv2.equalizeHist(frame[int(y + (h / 4)):(y + h), x:(x + w)])
+                cv2.line(frame, (x + w, y), ((x, y + h)), (0, 0, 255), 1) # create crosshairs
+                pupilFrame = cv2.equalizeHist(frame[int(y + (h / 4)):(y + h), x:(x + w)]) #make image more clear
                 pupilO = pupilFrame
                 ret, pupilFrame = cv2.threshold(pupilFrame, 55, 255, cv2.THRESH_BINARY)  # 50 ..nothin 70 is better
                 pupilFrame = cv2.adaptiveThreshold(pupilFrame, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 13, 2)
+                # make a threshold for white and dark
                 pupilFrame = cv2.morphologyEx(pupilFrame, cv2.MORPH_CLOSE, windowClose)
                 pupilFrame = cv2.morphologyEx(pupilFrame, cv2.MORPH_ERODE, windowErode)
                 pupilFrame = cv2.morphologyEx(pupilFrame, cv2.MORPH_OPEN, windowOpen)
@@ -52,7 +52,7 @@ def capturePupil(cxpts):
 
                 threshold = cv2.inRange(pupilFrame, 250, 255)  # get the blobs
                 _, contours, hierarchy = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
+                # find all the contours of the image
 
                 # if there are 3 or more blobs, delete the biggest and delete the left most for the right eye
                 # if there are 2 blob, take the second largest
@@ -88,14 +88,11 @@ def capturePupil(cxpts):
                         rect = cv2.boundingRect(cnt)
                         area = rect[0]*rect[1]
                         radius = rect[1] / 2
-                        sizeRate = 1.0 * rect[1] / len(pupilFrame[0])
-                        # if 0.25<=sizeRate<=0.41:
-                        #     print "area is {}, rect is {}, radius is {}, sizeRate is {}".format(area, rect, radius, sizeRate)
-
+                        sizeRate = 1.0 * rect[1] / len(pupilFrame[0]) #pupils take up a certain amount of space
                         if sizeRate >= 0.25 and sizeRate <= 0.41 and\
                             rect[0] != 0 and\
                             math.fabs(1 - 1.0*rect[1]/rect[0]) <= 0.5 and\
-                            area > maxArea:
+                            area > maxArea: # because of glare, the rectangle might be 2x * x, so 0.5
                             maxArea = area
                             largeBlob = cnt
 
@@ -105,12 +102,8 @@ def capturePupil(cxpts):
 
                     print(int(np.interp(cx, cxpts, xpts)))
 
-                    #print "cx is {}, cy is {}".format(cx, cy)
                     cv2.circle(pupilO, (cx, cy), 5, 255, -1)
 
-            # show picture
-            cv2.imshow('frame', pupilO)
-            # cv2.imshow('frame2', pupilFrame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
@@ -123,9 +116,9 @@ def capturePupil(cxpts):
 
 def calibrate():
     '''
-        spawns the pupil movement tracker and runs it
-        :return: nothing
-        '''
+    spawns the pupil movement tracker and runs it
+    :return: nothing
+    '''
     cap = cv2.VideoCapture(0)  # 640,480
     w = 640
     h = 480
@@ -202,9 +195,6 @@ def calibrate():
                         area = rect[0] * rect[1]
                         radius = rect[1] / 2
                         sizeRate = 1.0 * rect[1] / len(pupilFrame[0])
-                        # if 0.25<=sizeRate<=0.41:
-                        #     print "area is {}, rect is {}, radius is {}, sizeRate is {}".format(area, rect, radius, sizeRate)
-
                         if sizeRate >= 0.25 and sizeRate <= 0.41 and \
                                         rect[0] != 0 and \
                                         math.fabs(1 - 1.0 * rect[1] / rect[0]) <= 0.5 and \
@@ -215,8 +205,6 @@ def calibrate():
                 if largeBlob is not None and largeBlob.any() and len(largeBlob) > 0:
                     center = cv2.moments(largeBlob)
                     cx, cy = int(center['m10'] / center['m00']), int(center['m01'] / center['m00'])
-                    # print "cx is {}, cy is {}".format(cx, cy)
-                    # print(cx)
                     numcount += 1
                     if lravg == []:
                         lravg.append(cx)
@@ -228,9 +216,6 @@ def calibrate():
                     if(numcount == 3):
                         return [lravg[0]/3,lravg[1]/3]
 
-            # show picture
-            # cv2.imshow('frame', pupilO)
-            # cv2.imshow('frame2', pupilFrame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
--- runWizard.py
+++ runWizard.py
@@ -1,47 +1,51 @@
 import pupil
-import os
 from threading import Thread
 from pynput import mouse
 import cv2
-import numpy as np
 
 xpts = [30, 625, 1220]
-ypts = [30, 305, 611]
 cxpts = []
-cypts = []
 def on_click(x, y, button, pressed):
-    # print('{0} at {1}'.format(
-    #     'Pressed' if pressed else 'Released',
-    #     (x, y)))
+    '''
+    Determines if a click happens
+    :param x: x coordinate
+    :param y: y coordinate
+    :param button: button pressed
+    :param pressed: is the button pressed
+    :return: false if not pressed
+    '''
     if not pressed:
         # Stop listener
         return False
 
 def calib():
+    '''
+
+    :return:
+    '''
     numcalib = 0
     # Collect events until released
-    for _ in range(3):
+    for _ in range(3): # calibrate for all 3 points
         with mouse.Listener(on_click=on_click) as listener:
             listener.join()
             numcalib += 1
             longrun = pupil.calibrate()
             cxpts.append(longrun[0])
             cxpts.sort()
-            cypts.append(longrun[1])
-            cypts.sort()
 
 
 
 calib = Thread(target=calib)
-
+# use a thread to make sure this is independent
 calib.start()
 
 while calib.isAlive():
     x = 0
 
 cv2.destroyAllWindows()
-
+# make sure that nothing is left over
 cv2.waitKey(0)
 
 
 pupil.capturePupil(cxpts)
+# play the actual game
